13ec5dcc802d90f43d0a71f25ee68455911116d7
imhilla <hillaryodhiambo282@gmail.com>
2022-10-10 11:56:02 +0300

    insert into a list

---
+++ Chapter3/manipulate.py
@@ -0,0 +1,20 @@
+# changing, adding, removing
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+# motorcycles[0] = 'ducati'
+# print(motorcycles)
+
+# adding element
+motorcycles.append('ducati')
+print(motorcycles)
+
+empty_cycle_list = []
+empty_cycle_list.append('honda')
+empty_cycle_list.append('suzuki')
+empty_cycle_list.append('ducati')
+empty_cycle_list.append('yamaha')
+print(empty_cycle_list)
+
+# inserting a list
+# you can add a new element at any position in your list by using insert()
