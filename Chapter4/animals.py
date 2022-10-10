880c62aeb440fd46120cc2c0d5b098f3807fe933
imhilla <hillaryodhiambo282@gmail.com>
2022-10-10 20:50:06 +0300

    test list sum

---
+++ Chapter4/animals.py
@@ -0,0 +1,4 @@
+animal_lists = ['Cow', 'Goat', 'Chicken']
+for animal in animal_lists:
+    print(f"A {animal} would make a great animal")
+print('These animals would make a great pet')
---
+++ Chapter4/pizza.py
@@ -0,0 +1,4 @@
+pizza_lists = ['meat', 'chicken', 'peparoni']
+for pizza in pizza_lists:
+    print(f"I like {pizza} pizza")
+print('I really love pizza!')
