7fc56793b9e7ce5b122367619a9b5a6672752943
imhilla <hillaryodhiambo282@gmail.com>
2022-10-11 05:53:28 +0300

    use dictionaries

---
+++ Chapter6/alien.py
@@ -0,0 +1,13 @@
+alien_0 = {
+    'color': "green",
+    'points': 5
+}
+print(alien_0['color'])
+print(alien_0['points'])
+
+# dictionary is a collection of key value pair
+
+# adding new key value pairs
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
---
+++ Chapter6/dictionaries.py
@@ -0,0 +1,21 @@
+user_0 = {
+    'username': 'efermi',
+    'first': 'enrico',
+    'last': 'fermi',
+}
+
+for key, value in user_0.items():
+    print(key, value)
+
+favorite_languages = {
+    'jen': 'python',
+    'sarah': 'c',
+    'edward': 'ruby',
+    'phil': 'python',
+}
+
+for name in favorite_languages.keys():
+    print(name.title())
+
+for language in favorite_languages.values():
+    print(language.title())
