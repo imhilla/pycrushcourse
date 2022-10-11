062e9621e50d59c737275dff16ca7f618e185f13
imhilla <hillaryodhiambo282@gmail.com>
2022-10-11 04:02:56 +0300

    fifth commit

---
+++ Chapter4/count.py
@@ -0,0 +1,9 @@
+for number in range(1, 21):
+    print(number)
+
+# use comprehension to make list of numbers
+mil_numbers = [number for number in range(1, 1000001)]
+# for number in mil_numbers:
+#     print(number)
+# sum_mil = sum(mil_numbers)
+# print(sum_mil)
