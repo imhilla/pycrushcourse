e4803ca3b6bf54c37edc72d9de66d45e2b5aaa59
imhilla <hillaryodhiambo282@gmail.com>
2022-10-11 03:53:28 +0300

    use list comprehension

--- Chapter4/even_numbers.py
+++ Chapter4/even_numbers.py
@@ -1,2 +1,2 @@
-even_numbers = list(range(2, 100, 2))
+even_numbers = list(range(2, 100, 5))
 print(even_numbers)
--- Chapter4/squares.py
+++ Chapter4/squares.py
@@ -1,9 +1,13 @@
-squares = []
-for value in range(1, 11):
-    square = value**2
-    squares.append(square)
+# squares = []
+# for value in range(1, 11):
+#     square = value**2
+#     squares.append(square)
+# print(squares)
+# digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(max(digits))
+# print(min(digits))
+# print(sum(digits))
+
+# list comprehensions
+squares = [value**2 for value in range(1, 11)]
 print(squares)
-digits = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(max(digits))
-print(min(digits))
-print(sum(digits))
